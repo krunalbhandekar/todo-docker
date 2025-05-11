0de58d60bfdb83c17adfaff640c7359246d69e1c
Krunal Bhandekar <krunalbhandekar10@gmail.com>
2025-05-11 19:23:08 +0530

    feat(frontend):Added functionality to add and delete todo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Table, Button, Input, Row, Col, message } from "antd";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const SERVER_URL = "";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const fetchTodos = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(`${SERVER_URL}/todos`);
+      if (res.data.status === "error") {
+        messageApi.open({
+          type: "error",
+          content: res.data.error || "error",
+        });
+      } else {
+        setTodos(res.data.todos);
+      }
+    } catch (error) {
+      messageApi.open({
+        type: "error",
+        content: error.message || "error",
+      });
+    }
+    setLoading(false);
+  };
+
+  const addTodo = async () => {
+    if (!title) {
+      messageApi.open({ type: "error", content: "Please enter a valid Title" });
+    }
+    setLoading(true);
+    try {
+      const res = await axios.post(`${SERVER_URL}/todos`, { title });
+      if (res.data.status === "error") {
+        messageApi.open({
+          type: "error",
+          content: res.data.error || "error",
+        });
+      } else {
+        fetchTodos();
+      }
+    } catch (error) {
+      messageApi.open({
+        type: "error",
+        content: error.message || "error",
+      });
+    }
+    setLoading(false);
+    setTitle("");
+  };
+
+  const deleteTodo = async (id) => {
+    setLoading(true);
+    try {
+      const res = await axios.delete(`${SERVER_URL}/todos/${id}`);
+      if (res.data.status === "error") {
+        messageApi.open({
+          type: "error",
+          content: res.data.error || "error",
+        });
+      } else {
+        messageApi.open({
+          type: "success",
+          content: "Todo deleted successfully",
+        });
+        fetchTodos();
+      }
+    } catch (error) {
+      messageApi.open({
+        type: "error",
+        content: error.message || "error",
+      });
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      render: (id) => <span>{id}</span>,
+      width: 10,
+    },
+    {
+      title: "Title",
+      dataIndex: "title",
+      render: (title) => <span>{title}</span>,
+      width: 300,
+    },
+    {
+      title: "Action",
+      dataIndex: "sction",
+      render: (_, todo) => (
+        <Button danger onClick={() => deleteTodo(todo.id)}>
+          Delete
+        </Button>
+      ),
+      width: 20,
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: 20 }}>
+      {contextHolder}
+      <h1>TODO APP</h1>
+      <Row gutter={10} align="middle" justify="center">
+        <Col>
+          <Input
+            placeholder="Enter Todo"
+            style={{ marginRight: 5 }}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </Col>
+        <Col>
+          <Button type="primary" onClick={addTodo}>
+            Add Todo
+          </Button>
+        </Col>
+      </Row>
+      <div style={{ padding: 20 }}>
+        <Table
+          dataSource={todos}
+          columns={columns}
+          bordered
+          loading={loading}
+          size="small"
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
